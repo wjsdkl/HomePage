5b9d79eb38fd27a02a0255abf0200d5548fe16d1
XzaiCloud <xzai0313@qq.com>
2023-04-11 07:22:10 +0800

    6

--- gpt/assets/secret_config.js
+++
@@ -1,3 +0,0 @@
-// 公益api key，请换成自己的
-var api_keys = ['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', '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'];
-var my_api_key = api_keys[Math.floor(Math.random() * api_keys.length)];
