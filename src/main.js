401f803da9c7b5be53bbed1e09b9d6615781abe6
XzaiCloud <xzai0313@qq.com>
2023-06-13 18:51:06 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import 'element-plus/dist/index.css'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-export default MyApp;
-
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
